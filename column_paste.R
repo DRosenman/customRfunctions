de3edc24256de83d1a2e1c96b39aeec7b3ef91cc
Dave Rosenman <rosenmd1@tcnj.edu>
2018-02-03 09:45:18 -0500

    Add files via upload

--- column_paste.R
+++ column_paste.R
@@ -1,14 +1,15 @@
-library(tidyverse)
+library(tidyr)
 library(rlang)
+library(stringr)
 
-column_paste <- function(df,...,col, sep = ",") {
+paste_columns <- function(df,...,col, sep = ",") {
   #df is the dataframe
   #... the columns you want to combine
-  #colname is the name of the output column
+  #col is the name of the output column
   #sep is the character that will separate the combined column strings
   
   #returns:
-  # - A dataframe with the first column, "col", being a combination of the column ... separated by "sep" (default ",").
+  # - A dataframe with the first column, col , being a combination of the columns ... separated by "sep" (default ",").
   # - Note: If the values in each column for a row are NA, the output for that row in 'col' will be NA.
   #         If one or more, but not all, values of the combined columns in a row are NA, all NA's will be remove in 'col' for that row.   
   
@@ -25,19 +26,18 @@ column_paste <- function(df,...,col, sep = ",") {
   # 3 <NA>         V8      C
   # 4 <NA>        <NA>   <NA>
   
-  #> column_concat(df,x,y,z.xyz)
+  #> paste_columns(df,x,y,z,col = xyz)
   #       xyz               x          y      z
-  # 1 U1,U2,U3,V4,V5    U1,U2,U3     V4,V5    A
-  # 2     U1,U2          U1,U2        <NA>   <NA>
-  # 3      V8             <NA>         V8     C
+  # 1 U1,U2,U3,V4,V5,A    U1,U2,U3     V4,V5    A
+  # 2     U1,U2           U1,U2        <NA>   <NA>
+  # 3      V8,C           <NA>         V8     C
   # 4     <NA>            <NA>        <NA>   <NA>
   
   cols <- quos(...)
-  col <- enquo(colname)
+  col <- enquo(col)
   df <- df %>% unite(!!col,!!!cols,sep = sep,remove = FALSE)
   n <- length(cols) 
   na_string <- paste(replicate(n,"NA"), collapse = ",")
   df[,1] <- str_replace_all(df[,1],c(na_string = NA, "NA," = "", ",NA" = ""))
-  
-  return(df)
+  replace(df,df == "NA",NA)
 }
